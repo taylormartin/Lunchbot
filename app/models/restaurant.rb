75cdd0efa1c2a6a82adc2768c4f526c855df7819
Taylor Martin <taylormartin.uga@gmail.com>
2014-11-12 17:01:42 -0500

    passing model specs

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,14 @@
 class Restaurant < ActiveRecord::Base
 	validates :name, presence: true, uniqueness: true
 	has_many :reviews
+
+	def avg_rating
+		reviews = self.reviews
+		ratings = []
+		reviews.each do |rev|
+			ratings << rev.rating
+		end
+		avg = ratings.inject(0.0) { |sum, element| sum + element } / ratings.size
+	end
+
 end
